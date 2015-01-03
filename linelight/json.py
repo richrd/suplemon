40d4b649fc6cd08e42e20baab8ab7567793f4cee
Richard Lewis <richrd.lewis@gmail.com>
2015-01-03 19:18:24 +0200

    Added json linelighter.

---
+++ linelight/json.py
@@ -0,0 +1,21 @@
+from helpers import *
+
+def parse(raw_line):
+    color = 0
+    line = raw_line.strip()
+    if starts(line, ["{", "}"]):
+        #color = 11    # Blue
+        color = 17    # Blue
+    elif starts(line, "\""):
+        color = 13    # Green
+    #elif starts(line, "def"):
+    #    color = 12    # Cyan
+    #elif starts(line, ["return", "yield"]):
+    #    color = 15    # Red
+    #elif starts(line, "self."):
+    #    color = 13    # Cyan
+    #elif starts(line, ["#", "//", "\"", "'"]):
+    #    color = 14    # Magenta
+    #elif starts(line, ["if", "elif","else", "finally", "try", "except", "for ", "while ", "continue", "pass", "break"]):
+    #    color = 17    # Yellow
+    return color
