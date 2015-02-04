0adb8278d134084b066c4470854df8a0f29a651c
Richard Lewis <richrd.lewis@gmail.com>
2015-02-05 01:39:06 +0200

    Added config module that opens config file for editing.

---
+++ modules/config.py
@@ -0,0 +1,15 @@
+from mod_base import *
+ 
+class Config(Command):
+    def __init__(self):
+        pass
+ 
+    def run(self, app, editor):
+        if not app.open_file("~/.suplemon-config.json"):
+            app.new_file("~/.suplemon-config.json")
+        app.switch_to_file(app.last_file())
+ 
+module = {
+    "class": Config,
+    "name": "config",
+}
