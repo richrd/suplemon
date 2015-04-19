da8c0346675886acf64da222f6c51473cdc4a78a
Richard Lewis <richrd.lewis@gmail.com>
2015-04-19 19:25:00 +0300

    Example for binding key to run method of extension.

--- modules/rstrip.py
+++ modules/rstrip.py
@@ -1,6 +1,10 @@
 from mod_base import *
 
 class RStrip(Command):
+    def init(self):
+        #self.bind_key("^R") # Used to bind a key to the run-method
+        pass
+        
     def run(self, app, editor):
         line_nums = editor.get_lines_with_cursors()
         for n in line_nums:
