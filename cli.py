de0537c3defb1a1d92c265832c71c0853ffffecd
Richard Lewis <richrd.lewis@gmail.com>
2015-06-01 14:38:32 +0300

    Added Python < 2.7 support.

--- cli.py
+++ cli.py
@@ -3,8 +3,11 @@
 """
 Start a Suplemon instance in the current window
 """
-
-import argparse
+try:
+    import argparse
+except:
+    # Python < 2.7
+    argparse = False
 
 from main import App, __version__
 
@@ -12,13 +15,18 @@ from main import App, __version__
 def main():
     """Handle CLI invocation"""
     # Parse our CLI arguments
-    parser = argparse.ArgumentParser(description="Console text editor with multi cursor support")
-    parser.add_argument("filenames", metavar="filename", type=str, nargs="*", help="Files to load into Suplemon")
-    parser.add_argument("--version", action="version", version=__version__)
-    args = parser.parse_args()
+    if argparse:
+        parser = argparse.ArgumentParser(description="Console text editor with multi cursor support")
+        parser.add_argument("filenames", metavar="filename", type=str, nargs="*", help="Files to load into Suplemon")
+        parser.add_argument("--version", action="version", version=__version__)
+        args = parser.parse_args()
+        filenames = args.filenames
+    else:
+        # Python < 2.7 fallback
+        filenames = sys.argv[1:]
 
     # Generate and start our application
-    app = App(filenames=args.filenames)
+    app = App(filenames=filenames)
     app.init()
 
     # Output log info
