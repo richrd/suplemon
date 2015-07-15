01956729d2c0c147fde01cdb96471ce5669ef122
Richard Lewis <richrd.lewis@gmail.com>
2015-07-15 14:09:24 +0300

    New event binders.

--- mod_base.py
+++ mod_base.py
@@ -1,16 +1,20 @@
-#-*- encoding: utf-8
+# -*- encoding: utf-8
 """
 Base class for extension modules to inherit.
 """
 from helpers import *
 
-#TODO: rename to Module and add subclass Command if needed
+
 class Command:
+    # TODO: rename to Module and add subclass Command if needed
     def __init__(self, app):
         self.app = app
         self.init()
 
     def init(self):
+        pass
+
+    def run(self, app, editor):
         pass
 
     def log(self, data):
@@ -19,10 +23,18 @@ class Command:
     def bind_key(self, key):
         """Shortcut for binding run method to a key."""
         self.app.set_key_binding(key, self._proxy_run)
-        
+
     def bind_event(self, event, callback):
         """Binding a method to an event."""
-        self.app.set_event_binding(event, callback)
-        
+        self.app.set_event_binding(event, "before", callback)
+
+    def bind_event_before(self, event, callback):
+        """Binding a method to an event."""
+        self.app.set_event_binding(event, "before", callback)
+
+    def bind_event_after(self, event, callback):
+        """Binding a method to an event."""
+        self.app.set_event_binding(event, "after", callback)
+
     def _proxy_run(self):
         self.run(self.app, self.app.get_editor())
