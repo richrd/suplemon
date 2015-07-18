579d68d5caff4d0502976c0be8d9815b8916c13a
Richard Lewis <richrd.lewis@gmail.com>
2015-07-18 12:37:21 +0300

    Fixed checking linting data.

--- suplemon/modules/linter.py
+++ suplemon/modules/linter.py
@@ -21,53 +21,6 @@ class Linter(Module):
         self.bind_event_after("reload_file", self.lint_current_file)
         self.bind_event_after("open_file", self.lint_current_file)
 
-    def has_flake8_support(self):
-        return self.get_output(["flake8"])
-
-    def lint_current_file(self, event):
-        self.lint_file(self.app.get_file())
-
-    def lint_all_files(self, event):
-        """Do linting check for all open files and store results."""
-        for file in self.app.files:
-            self.lint_file(file)
-        return False
-
-    def lint_file(self, file):
-        path = file.get_path()
-        if not path:  # Unsaved file
-            return False
-        if file.get_extension().lower() != "py":  # Only lint Python files
-            return False
-        linting = self.get_file_linting(path)
-        if not linting:  # Linting failed
-            return False
-        editor = file.get_editor()
-        line_no = 0
-        while line_no < len(editor.lines):
-            line = editor.lines[line_no]
-            if line_no+1 in linting.keys():
-                line.linting = linting[line_no+1]
-                line.set_number_color(1)
-            else:
-                line.linting = False
-                line.reset_number_color()
-            line_no += 1
-
-    def get_msgs_on_line(self, editor, line_no):
-        line = editor.lines[line_no]
-        if not hasattr(line, "linting") or not line.linting:
-            return False
-        return line.linting[0][1]
-
-    def get_msg_count(self, editor):
-        count = 0
-        for line in editor.lines:
-            if hasattr(line, "linting"):
-                if line.linting:
-                    count += 1
-        return count
-
     def run(self, app, editor):
         """Run the linting command."""
         editor = self.app.get_file().get_editor()
@@ -87,19 +40,68 @@ class Linter(Module):
         if msg:
             self.app.set_status("Line " + str(line_no) + ": " + msg)
 
+    def has_flake8_support(self):
+        return self.get_output(["flake8"])
+
+    def lint_current_file(self, event):
+        self.lint_file(self.app.get_file())
+
+    def lint_all_files(self, event):
+        """Do linting check for all open files and store results."""
+        for file in self.app.files:
+            self.lint_file(file)
+        return False
+
+    def lint_file(self, file):
+        path = file.get_path()
+        if not path:  # Unsaved file
+            return False
+        if file.get_extension().lower() != "py":  # Only lint Python files
+            return False
+        linting = self.get_file_linting(path)
+        if linting is False:  # Linting failed
+            return False
+        editor = file.get_editor()
+        line_no = 0
+        while line_no < len(editor.lines):
+            line = editor.lines[line_no]
+            if line_no+1 in linting.keys():
+                line.linting = linting[line_no+1]
+                line.set_number_color(1)
+            else:
+                line.linting = False
+                line.reset_number_color()
+            line_no += 1
+
     def get_output(self, cmd):
         try:
             process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         except EnvironmentError:
             # cant use FileNotFoundError in Python 2
+            self.logger.exception("Getting command output failed.")
             return False
         out, err = process.communicate()
         return out
 
+    def get_msgs_on_line(self, editor, line_no):
+        line = editor.lines[line_no]
+        if not hasattr(line, "linting") or not line.linting:
+            return False
+        return line.linting[0][1]
+
+    def get_msg_count(self, editor):
+        count = 0
+        for line in editor.lines:
+            if hasattr(line, "linting"):
+                if line.linting:
+                    count += 1
+        return count
+
     def get_file_linting(self, path):
         """Do linting check for given file path."""
         output = self.get_output(["flake8", path])
-        if not output:
+        if output is False:
+            self.logger.warning("Failed to get linting for file '{}'.".format(path))
             return False
         output = output.decode("utf-8")
         # Remove file paths from output
