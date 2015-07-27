c436c7018ff458caeb32a9d2969c4fc21ecdacef
Richard Lewis <richrd.lewis@gmail.com>
2015-07-27 15:01:12 +0300

    Failure tolerance etc.

--- suplemon/modules/eval.py
+++ suplemon/modules/eval.py
@@ -5,8 +5,16 @@ from suplemon_module import Module
 
 class Eval(Module):
     def run(self, app, editor, args):
-        value = eval(args)
-        app.set_status("Eval:{}".format(value))
+        if not args:
+            app.set_status("Please enter an expression to evaluate.")
+            return False
+        try:
+            value = eval(args)
+        except:
+            app.set_status("Eval failed.")
+            return False
+        app.set_status("Result:{}".format(value))
+        return True
 
 module = {
     "class": Eval,
