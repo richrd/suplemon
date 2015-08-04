f99dd80c3cf2efa48661e7a8c17629eec15171ae
Richard Lewis <richrd.lewis@gmail.com>
2015-08-04 11:08:39 +0300

    Implemented direct evaluation of line contents.

--- suplemon/modules/eval.py
+++ suplemon/modules/eval.py
@@ -6,15 +6,28 @@ from suplemon.suplemon_module import Module
 class Eval(Module):
     def run(self, app, editor, args):
         if not args:
-            app.set_status("Please enter an expression to evaluate.")
-            return False
+            return self.evaluate_lines(editor)
+        else:
+            return self.evaluate_input(args)
+
+    def evaluate_input(self, inp):
         try:
-            value = eval(args)
+            value = eval(inp)
         except:
-            app.set_status("Eval failed.")
+            self.app.set_status("Eval failed.")
             return False
-        app.set_status("Result:{0}".format(value))
+        self.app.set_status("Result:{0}".format(value))
         return True
+
+    def evaluate_lines(self, editor):
+        line_nums = editor.get_lines_with_cursors()
+        for num in line_nums:
+            line = editor.get_line(num)
+            try:
+                value = eval(line.get_data())
+            except:
+                continue
+            line.set_data(str(value))
 
 module = {
     "class": Eval,
