b27616c486a3a4ffffed23f5cfbc624577bb018e
Richard Lewis <richrd.lewis@gmail.com>
2015-08-04 11:07:55 +0300

    Added convenience launcher script.

---
+++ suplemon.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Convenience wrapper for running suplemon directly from source tree."""
+
+from suplemon.cli import main
+
+if __name__ == '__main__':
+    main()
