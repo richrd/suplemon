e4a834ed07ce28226b179036623c81d65aef537b
Richard Lewis <richrd.lewis@gmail.com>
2015-12-16 14:43:47 +0200

    Added linting support for JavaScript via jslint.

--- suplemon/modules/linter.py
+++ suplemon/modules/linter.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8
 
+import re
 import subprocess
 
 from suplemon.suplemon_module import Module
@@ -8,14 +9,6 @@ from suplemon.suplemon_module import Module
 class Linter(Module):
     def init(self):
         self.init_logging(__name__)
-        if not self.has_flake8_support():
-            self.logger.warning("Flake8 not available. Can't show linting.")
-            return False
-
-        # Error codes to ignore e.g. 'E501' (line too long)
-        self.ignore = []
-        # Max length of line
-        self.max_line_length = 120  # Default is 79
 
         # TODO: Run linting in a seperate thread to avoid
         # blocking the UI when the app is loading
@@ -49,10 +42,6 @@ class Linter(Module):
         if msg:
             self.app.set_status("Line " + str(line_no) + ": " + msg)
 
-    def has_flake8_support(self):
-        output = self.get_output(["flake8", "--version"])
-        return output
-
     def lint_current_file(self, event):
         self.lint_file(self.app.get_file())
 
@@ -66,11 +55,17 @@ class Linter(Module):
         path = file.get_path()
         if not path:  # Unsaved file
             return False
-        if file.get_extension().lower() != "py":  # Only lint Python files
+
+        ext = file.get_extension().lower()
+        if ext == "py":
+            linter = PyLint(self.logger)
+        elif ext == "js":
+            linter = JsLint(self.logger)
+
+        linting = linter.lint(path)
+        if linting is False:
             return False
-        linting = self.get_file_linting(path)
-        if linting is False:  # Linting failed
-            return False
+
         editor = file.get_editor()
         line_no = 0
         while line_no < len(editor.lines):
@@ -82,14 +77,6 @@ class Linter(Module):
                 line.linting = False
                 line.reset_number_color()
             line_no += 1
-
-    def get_output(self, cmd):
-        try:
-            process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        except (OSError, EnvironmentError):  # can't use FileNotFoundError in Python 2
-            return False
-        out, err = process.communicate()
-        return out
 
     def get_msgs_on_line(self, editor, line_no):
         line = editor.lines[line_no]
@@ -105,14 +92,53 @@ class Linter(Module):
                     count += 1
         return count
 
+
+class BaseLint:
+    def __init__(self, logger):
+        self.logger = logger
+
+    def lint(self, path):
+        pass
+
+    def get_file_linting(self, path):
+        """Do linting check for given file path."""
+        return {}
+
+    def get_output(self, cmd):
+        try:
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        except (OSError, EnvironmentError):  # can't use FileNotFoundError in Python 2
+            return False
+        out, err = process.communicate()
+        return out
+
+
+class PyLint(BaseLint):
+    def __init__(self, logger):
+        BaseLint.__init__(self, logger)
+        # Error codes to ignore e.g. 'E501' (line too long)
+        self.ignore = []
+        # Max length of line
+        self.max_line_length = 120  # Default is 79
+
+    def lint(self, path):
+        if not self.has_flake8_support():
+            self.logger.warning("Flake8 not available. Can't show linting.")
+            return False
+        return self.get_file_linting(path)
+
+    def has_flake8_support(self):
+        output = self.get_output(["flake8", "--version"])
+        return output
+
     def get_file_linting(self, path):
         """Do linting check for given file path."""
         output = self.get_output(["flake8", "--max-line-length", str(self.max_line_length), path])
-
         if output is False:
             self.logger.warning("Failed to get linting for file '{0}'.".format(path))
             return False
         output = output.decode("utf-8")
+        self.logger.debug(output)
         # Remove file paths from output
         output = output.replace(path+":", "")
         lines = output.split("\n")
@@ -120,18 +146,63 @@ class Linter(Module):
         for line in lines:
             if not line:
                 continue
-            parts = line.split(":")
-            line_no = int(parts[0])
-            char_no = int(parts[1])
-            data = ":".join(parts[2:]).strip()
-            err_code = data.split(" ")[0]
-            if err_code in self.ignore:
-                continue
-            if line_no not in linting.keys():
-                linting[line_no] = []
-            linting[line_no].append((char_no, data, err_code))
+            try:
+                parts = line.split(":")
+                line_no = int(parts[0])
+                char_no = int(parts[1])
+                data = ":".join(parts[2:]).strip()
+                err_code = data.split(" ")[0]
+                if err_code in self.ignore:
+                    continue
+                if line_no not in linting.keys():
+                    linting[line_no] = []
+                linting[line_no].append((char_no, data, err_code))
+            except:
+                self.logger.debug("Failed to parse line:{}".format(line))
         return linting
 
+
+class JsLint(BaseLint):
+    def __init__(self, logger):
+        BaseLint.__init__(self, logger)
+
+    def lint(self, path):
+        return self.get_file_linting(path)
+
+    def get_file_linting(self, path):
+        """Do linting check for given file path."""
+        output = self.get_output(["jshint", path])
+        if output is False:
+            self.logger.warning("Failed to get linting for file '{0}'.".format(path))
+            return False
+        output = output.decode("utf-8")
+        # Remove file paths from output
+        output = output.replace(path+": ", "")
+        self.logger.debug(output)
+        lines = output.split("\n")
+        linting = {}
+        for line in lines:
+            self.logger.debug(line)
+            if not line:
+               continue
+            try:
+                parts = line.split(", ")
+                if len(parts) < 3:
+                    continue
+                self.logger.debug("PARTS")
+                self.logger.debug(parts)
+                line_no = int(re.sub("\D", "", parts[0]))
+                char_no = int(re.sub("\D", "", parts[1]))
+                data = parts[2]
+                err_code = None
+                if line_no not in linting.keys():
+                    linting[line_no] = []
+                linting[line_no].append((char_no, data, err_code))
+            except:
+                self.logger.debug("Failed to parse line:{}".format(line))
+        return linting
+
+ # asd
 module = {
     "class": Linter,
     "name": "linter",
