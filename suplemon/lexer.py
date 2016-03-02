12b336e99ad62c2eecbb15bafdf8d648041270a7
Richard Lewis <richrd.lewis@gmail.com>
2016-03-03 01:54:44 +0200

    Slightly optimized lexer.

--- suplemon/lexer.py
+++ suplemon/lexer.py
@@ -1,12 +1,24 @@
 # -*- encoding: utf-8
 
 import pygments
+import pygments.token
 import pygments.lexers
 
 
 class Lexer:
     def __init__(self, app):
         self.app = app
+        self.token_map = {
+            pygments.token.Comment: "comment",
+            pygments.token.Comment.Single: "comment",
+            pygments.token.Name.Function: "entity.name.function",
+            pygments.token.Name.Class: "entity.name.class",
+            pygments.token.Name.Tag: "entity.name.tag",
+            pygments.token.Name.Attribute: "entity.other.attribute-name",
+            pygments.token.Name.Variable: "variable",
+            pygments.token.Operator: "keyword",
+            pygments.token.Name.Builtin.Pseudo: "constant.language",
+        }
 
     def lex(self, code, lex):
         """Return tokenified code.
@@ -31,32 +43,14 @@ class Lexer:
             token = word[0]
             scope = "global"
 
-            if token in pygments.token.Keyword:
-                scope = "keyword"
-            elif token == pygments.token.Comment:
-                scope = "comment"
-            elif token == pygments.token.Comment.Single:
-                scope = "comment"
-            elif token == pygments.token.Name.Function:
-                scope = "entity.name.function"
-            elif token == pygments.token.Name.Class:
-                scope = "entity.name.class"
-            elif token == pygments.token.Name.Tag:
-                scope = "entity.name.tag"
-            elif token == pygments.token.Name.Attribute:
-                scope = "entity.other.attribute-name"
-            elif token == pygments.token.Name.Variable:
-                scope = "variable"
-            elif token == pygments.token.Operator:
-                scope = "keyword"
-            elif token == pygments.token.Name.Builtin.Pseudo:
-                scope = "constant.language"
-            elif token in pygments.token.Literal.String:
-                scope = "string"
+            if token in self.token_map.keys():
+                scope = self.token_map[token]
             elif token in pygments.token.Literal.Number:
                 scope = "constant.numeric"
             elif token in pygments.token.Name:
                 scope = "entity.name"
+            elif token in pygments.token.Keyword:
+                scope = "keyword"
 
             scopes.append((scope, word[1]))
         return scopes
