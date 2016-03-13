b26cf3b5924f104fc79a8a4360873efffea6a04e
Richard Lewis <richrd.lewis@gmail.com>
2016-03-13 19:32:11 +0200

    Revert disabling version check.

--- release.sh
+++ release.sh
@@ -14,10 +14,10 @@ fi
 sed -E "s/^(__version__ = \")[0-9]+\.[0-9]+\.[0-9]+(\")$/\1$version\2/" suplemon/main.py --in-place
 
 # Verify our version made it into the file
-#if ! grep "$version" suplemon/main.py &> /dev/null; then
-#  echo "Expected \`__version__\` to update via \`sed\` but it didn't" 1>&2
-#  exit 1
-#fi
+if ! grep "$version" suplemon/main.py &> /dev/null; then
+  echo "Expected \`__version__\` to update via \`sed\` but it didn't" 1>&2
+  exit 1
+fi
 
 # Commit the change
 git add suplemon/main.py
