ec6a3689dc4a0e32cf1373ea78087aacb59f6a4c
Richard Lewis <richrd.lewis@gmail.com>
2017-03-25 01:37:51 +0200

    Initial main and screen implementations.

---
+++ suplemon/main.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8
+
+"""
+The main class that starts and runs Suplemon.
+"""
+
+
+__version__ = "0.1.90"
+
+
+from .logger import logger
+from .backends.curses import CursesBackend
+
+
+class App:
+    def __init__(self, filenames=None, config_file=None):
+        self.debug = 1
+        self.logger = logger
+        self.logger.debug("__init__")
+
+        # EXPERIMENTAL
+        self.backend = CursesBackend()
+
+    def init(self):
+        self.logger.debug("init")
+        self.backend.init()
+        return True
+
+    def run(self):
+        self.logger.debug("run")
+        self.backend.start()
+
+    def shutdown(self):
+        self.backend.stop()
+        self.logger.debug("shutdown")
+
+        if self.debug:
+            for logger_handler in self.logger.handlers:
+                logger_handler.close()
