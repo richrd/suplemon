7a2e065d73da1ba4105864515c22d1b97dac3af4
Consolatis <35009135+Consolatis@users.noreply.github.com>
2018-02-10 23:36:29 +0000

    [WIP] improve limit config option for filelist module, change default to center: False, rotate: False, limit: 5

--- suplemon/modules/filelist.py
+++ suplemon/modules/filelist.py
@@ -34,30 +34,45 @@ class FileListGenerator(StatusComponentGenerator):
 
         use_unicode = self.app.config["app"]["use_unicode_symbols"]
         show_modified = self.app.config["display"]["show_file_modified_indicator"]
+        style_other = self.app.ui.colors.get("filelist_other")
+        style_active = self.app.ui.colors.get("filelist_active")
 
         config = self.app.config["modules"][__name__]
         no_write_symbol = config["no_write"][use_unicode]
         is_changed_symbol = config["is_changed"][use_unicode]
         rotate = config["rotate"]
+        center = config["center"]
         wrap_active = config["wrap_active"]
         wrap_active_align = config["wrap_active_align"]
         limit = config["limit"]
-        style_other = self.app.ui.colors.get("filelist_other")
-        style_active = self.app.ui.colors.get("filelist_active")
 
         files = self.app.get_files()
         curr_file_index = self.app.current_file_index()
         curr_file = files[curr_file_index]
         filecount = len(files)
-        if 0 < limit < filecount:
-            files = [
-                files[curr_file_index - 1],
-                files[curr_file_index],
-                files[(curr_file_index + 1) % filecount]
-            ]
-        elif rotate:
-            # TODO: allow rotate if hitting limit as well?
+
+        if rotate:
+            # Active file on front
             files = files[curr_file_index:] + files[:curr_file_index]
+        elif center and filecount > 2:
+            # Active file in the middle
+            if 0 < limit < filecount:
+                if limit % 2 == 0:
+                    limit -= 1
+                offset = (limit - 1) // 2
+            else:
+                if filecount % 2 == 0:
+                    limit = filecount - 1
+                offset = (filecount - 1) // 2
+            files = [
+                files[x % filecount] for x in range(curr_file_index - offset, curr_file_index + offset + 1)
+            ]
+        elif 0 < limit < filecount:
+            # Try starting at index 0 but ensure active file always visible
+            files = files[max(0, curr_file_index - (limit - 1)):]
+
+        if not center and 0 < limit < filecount:
+            files = files[:limit]
 
         for f in files:
             name = f.name
@@ -87,12 +102,13 @@ class FileList(Module):
 
     def get_default_config(self):
         return {
+            "center": False,
             "rotate": False,
             "wrap_active": True,
             "wrap_active_align": False,
             "no_write": ["!", "\u2715"],
             "is_changed": ["*", "\u2732"],
-            "limit": 0
+            "limit": 5
         }
 
 
